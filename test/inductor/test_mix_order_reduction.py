b2b616fa5e4f7095c2cdee067f440acaaaca5c6f
Benjamin Glass <bglass@openteams.com>
2026-04-14 14:33:45 +0000

    [inductor] Re-enable split reductions when cooperative reductions won't be selected
    Removes a performance hit (and several test breakages) associated with enabling the `cooperative_reductions` feature flag. This issue was because split reductions were accidentally unconditionally disabled on Triton if the feature flag was enabled, even for kernels where cooperative reductions would not be codegenned. This PR checks that cooperative reductions will be codegenned before disabling split reductions.
    
    
    ghstack-source-id: 175158916d8c49b592a887c7db4e95d65dbaff5a
    Pull-Request: https://github.com/pytorch/pytorch/pull/179859

--- test/inductor/test_mix_order_reduction.py
+++ test/inductor/test_mix_order_reduction.py
@@ -773,6 +773,13 @@ class MixOrderReductionTest(TestBase):
         self.assertEqual(len(compile_metrics), 1, "Don't recompile")
 
     @skipIfXpu(msg="https://github.com/intel/intel-xpu-backend-for-triton/issues/6398")
+    # Cooperative reductions cause this test to fail if Triton is enabled.
+    @inductor_config.patch(
+        {
+            "triton.cooperative_reductions": False,
+            "triton.force_cooperative_reductions": False,
+        }
+    )
     def test_additive_rnumel(self):
         """
         Fix https://github.com/pytorch/pytorch/issues/176375
--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -2872,7 +2872,9 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
 
     def should_use_cooperative_reduction(self) -> bool:
         return self.inside_reduction and V.choices.should_use_cooperative_reduction(
-            self.features
+            V.graph.get_current_device_or_throw(),
+            self.features.numel,
+            self.features.reduction_numel,
         )
 
     def init_cooperative_reduction(self):
@@ -4952,7 +4954,6 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
         Perform an associative scan on 'values'.
         """
         assert self.inside_reduction
-        assert not self.cooperative_reduction, "TODO"
         masks = OrderedSet(f"{tree.prefix}mask" for tree in self.range_trees)
         self.filter_masks(masks)
         masks = sorted(masks)
@@ -5077,7 +5078,6 @@ class TritonKernel(SIMDKernel[TritonCSEVariable]):
         descending: bool,
     ) -> tuple[CSEVariable, ...]:
         assert self.inside_reduction
-        assert not self.cooperative_reduction, "TODO"
         masks = OrderedSet(f"{tree.prefix}mask" for tree in self.range_trees)
         self.filter_masks(masks)
         masks = sorted(masks)
@@ -6716,10 +6716,6 @@ class TritonScheduling(SIMDScheduling):
             from .triton_split_scan import TritonSplitScanKernel
 
             kernel_type = TritonSplitScanKernel
-
-        if is_scan:
-            # TODO(jansel): scan does not yet work with cooperative reductions
-            kernel_kwargs["override_cooperative_reduction"] = False
 
         # ops.sort only works with persistent reduction, and is not bandwidth bound anyway
         # so taking the hit of non-coalesced loads is okay
